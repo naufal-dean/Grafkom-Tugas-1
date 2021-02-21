87f8902fe82e3c491713ac128488674b8f9821b4
naufal-dean <naufal.dean@gmail.com>
2021-02-21 20:09:55 +0700

    Fix model mismatch parent constructor call

--- model.js
+++ model.js
@@ -4,24 +4,26 @@ const LINE_MODEL = "line";
 const SQUARE_MODEL = "square";
 const POLYGON_MODEL = "polygon";
 
-function Model(drawMode, vertices, colors, vertexCount) {
+const DEFAULT_VERTEX_COLOR = [1.0, 1.0, 1.0, 1.0];
+
+function Model(drawMode, vertices = [], colors = [], vertexCount = 0) {
   this.vertices = vertices;
   this.colors = colors;
   this.vertexCount = vertexCount;
   this.drawMode = drawMode;
 }
 
-function Line(drawMode, vertices, colors) {
-  Model.call(this, vertices, colors, 2);
+function Line(drawMode, vertices = [], colors = []) {
+  Model.call(this, drawMode, vertices, colors, 2);
   this.type = LINE_MODEL;
 }
 
-function Square(drawMode, vertices, colors) {
-  Model.call(this, vertices, colors, 4);
+function Square(drawMode, vertices = [], colors = []) {
+  Model.call(this, drawMode, vertices, colors, 4);
   this.type = SQUARE_MODEL;
 }
 
-function Polygon(drawMode, vertices, colors, vertexCount) {
-  Model.call(this, vertices, colors, vertexCount);
+function Polygon(drawMode, vertices = [], colors = [], vertexCount = 0) {
+  Model.call(this, drawMode, vertices, colors, vertexCount);
   this.type = POLYGON_MODEL;
 }
